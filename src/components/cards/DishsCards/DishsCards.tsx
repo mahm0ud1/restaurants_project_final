f8ccaacbaafd800e038a4cc30b0be82318e562b7
mahmoud saleh <mahmoudsalehse@gmail.com>
2022-11-20 20:54:49 +0200

    add files

---
+++ src/components/cards/DishsCards/DishsCards.tsx
@@ -0,0 +1,13 @@
+const DishsCards = () => {
+    return (
+        <>
+            <div>
+                <div>popular restaurant in epicure:</div>
+                <div>Cards</div>
+                <div>{"All Restaurants >>"}</div>
+            </div>
+        </>
+    );
+}
+
+export default DishsCards;
---
+++ src/components/pages/HeroPicture/HeroPicture.tsx
@@ -0,0 +1,14 @@
+import './HeroPicture.css';
+
+const HeroPicture = () => {
+    return (
+        <>
+        <div className="hero_container">
+            <div>Test</div>
+            <div>Test</div>
+        </div>
+        </>
+    );
+}
+
+export default HeroPicture;
